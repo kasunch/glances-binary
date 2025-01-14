017bf39374d57753d459b6758887916b2cc1cf68
Kasun Hewage <kasun.ch@gmail.com>
2025-01-14 16:25:04 +0100

    Separate file for hidden imports
    * As it is not trivial to determine hidden imports automatically,
      a separate file is used to specify the hidden imports.
    
    * Renamed the file used to get version information appropriately.

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ SCRIPT_DIR_FULL=$(readlink -f "${SCRIPT_DIR}")
 PYTHON_VENV="build-env"
 BUILD_DIR="${SCRIPT_DIR_FULL}/build"
 PYTHON_VENV_DIR="${BUILD_DIR}/venv/${PYTHON_VENV}"
-REQUIREMENTS_FILE="${BUILD_DIR}/requirements.txt"
-RELEASE_NOTES_FILE="${BUILD_DIR}/release-notes.txt"
+HIDDEN_IMPORTS_FILE="${SCRIPT_DIR_FULL}/hidden-imports.txt"
+VERSION_FILE="${BUILD_DIR}/version.txt"
 
 mkdir -p "${SCRIPT_DIR_FULL}"
 mkdir -p "${PYTHON_VENV_DIR}"
@@ -19,22 +19,18 @@ mkdir -p "${PYTHON_VENV_DIR}"
 python -m venv "${PYTHON_VENV_DIR}"
 PATH="${PYTHON_VENV_DIR}/bin:${PATH}"
 
-pip install --no-cache-dir glances[all]
-
-pip freeze >"${REQUIREMENTS_FILE}"
-
-pip install --no-cache-dir pyinstaller
+pip install --no-cache-dir pyinstaller glances[all]
 
 pyinstaller_cmd="pyinstaller --onefile $(which glances) "
 pyinstaller_cmd+="--add-data ${PYTHON_VENV_DIR}/lib/python3.11/site-packages/glances:glances "
 
-while IFS= read -r package; do
-    package_name=$(echo "${package}" | cut -d'=' -f1)
-    pyinstaller_cmd+="--hidden-import=${package_name} "
-done <"${REQUIREMENTS_FILE}"
+while IFS= read -r module || [[ -n "${module}" ]]; do
+    module_name=$(echo "${module}" | xargs)
+    pyinstaller_cmd+="--hidden-import=${module_name} "
+done <"${HIDDEN_IMPORTS_FILE}"
 
 echo "Running command: ${pyinstaller_cmd}"
 
 cd ${BUILD_DIR} && ${pyinstaller_cmd}
 
-glances --version | grep -v "Log file" >"${RELEASE_NOTES_FILE}"
+glances --version | grep -v "Log file" >"${VERSION_FILE}"
